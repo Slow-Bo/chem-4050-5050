6d5b91dc87611078e75b29cea60c595eaaef1949
Slow-Bo <patrickrandolph1477@gmail.com>
2025-10-15 00:36:27 -0400

    I also needed a new Cv and U graph

--- homework-4-grad/plot_heat_capa.py
+++ homework-4-grad/plot_heat_capa.py
@@ -1,4 +1,5 @@
 #importing mat plot lib and pandas since I want to try to do this from a CSV
+#The main reason I'm importing a CSV and not the functions themselves is that I don't want to run that hard drive frier again!
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -18,15 +19,15 @@ Heat_Capacity = df['Heat Capacity']
 #prepares to plot two subplots, the way the matplotlib docs told me to
 fig, ax1 = plt.subplots()
 # Plot units for U on the left y-axis
-ax1.plot(Temperature, Internal_Energy, linestyle = '-', label='Internal Energy (U)', color= 'red')
+ax1.plot(Temperature, Internal_Energy, linestyle = '-', color= 'red')
 ax1.set_xlabel('Temperature (K)')
-ax1.set_ylabel('Internal Energy (eV)', color='red')
+ax1.set_ylabel('Internal Energy U (eV)', color='red')
 ax1.tick_params(axis='y', labelcolor='red')
 
 #Create a second y-axis for the Heat capacity
 ax2 = ax1.twinx()
-ax2.plot(Temperature, Heat_Capacity, linestyle = '-', label='Heat Capacity (Cv)', color= 'blue')
-ax2.set_ylabel('Heat Capacity (eV/K)', color='blue')
+ax2.plot(Temperature, Heat_Capacity, linestyle = '-', color= 'blue')
+ax2.set_ylabel('Heat Capacity C_v (eV/K)', color='blue')
 ax2.tick_params(axis='y', labelcolor='blue')
 
 #Sets the legend to the upper left so it's out of the way
