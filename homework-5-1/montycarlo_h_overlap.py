edf61b3cd72d74b8cd4220029ce271ffccb8ea0f
Slow-Bo <patrickrandolph1477@gmail.com>
2025-10-20 15:10:39 -0400

    Made it so function now actually uses exponential calculations, if only my laptop was powerful enough

--- homework-5-1/montycarlo_h_overlap.py
+++ homework-5-1/montycarlo_h_overlap.py
@@ -1,8 +1,8 @@
 #Imports all the functions I can't code as well as a few constants
 import numpy as np
 import matplotlib.pyplot as plt
-#Importing the exponentnal function since beta did not work.
-from scipy.stats import expon
+#Importing the trapazoid distribution since beta and exponentnal did not work.
+from scipy.stats import trapezoid
 
 #Setting the directory to our file so I can find the rest of my code, and so I save the graphs to the disired spot
 #Dang I must have been really frustrated when microsoft's stupid CoPilot AI told me how to do this
@@ -73,7 +73,7 @@ def importance_vision():
 #importance_vision()
 
 #Interestingly the function tends to peak around 2.5, is negitive around 0 and flatlines at 0 past 10 I need a function of similar shape
-#I have found a distribution I beileve will work for this. The beta distribution, but the exponental distribution is close enough.
+#After trial and error I have found that I can't get beta distribution to work, exponitial distribution would overprioritize the first part of the graph, so now I'll try trapazoid rule.
 
 #A function to calculate the monty carlo overlap, because all the variables are defined outside, I can just copy and paste.
 def Better_Overlap(R,L,seed,n_point):
@@ -81,27 +81,36 @@ def Better_Overlap(R,L,seed,n_point):
     #Set the random seed for reproducibility
     np.random.seed(seed)
 
-    #Sets the integraton limits from 0 to L since the orbital should be semetrical on all axises? axii? Plural of axis!
+    #Seting the bounds of integration
     a = 0
     b = L
+
+    #The way trapazoid works acording to the scipy docs is that I need to difine what percent of the way to L the trapazoid peaks, and where it starts going down again.
+    #Based on my importance_vision() this is what I chose.
+    c = 0.01
+    d = 0.25
 
     #Creates a random array of X,Y, and Z points from the bounds 0 to L
     #If I did this right it should follow the beta distribution and be more acurate
     
-    #It did not in fact work, as it turns out beta has 0 outside of its domain a lesser man would give up and use the exponental function since it should be close enough.
+    #It did not in fact work, as it turns out beta has 0 outside of its domain a lesser man would give up and use another function since it should be close enough.
     #I am a lesser man.
-    x = expon.rvs(size=n_point, scale=1)
-    y = expon.rvs(size=n_point, scale=1)
-    z = expon.rvs(size=n_point, scale=1)
+    x = trapezoid.rvs(c,d,loc=0,size=n_point, scale=L)
+    y = trapezoid.rvs(c,d,loc=0,size=n_point, scale=L)
+    z = trapezoid.rvs(c,d,loc=0,size=n_point, scale=L)
 
     #Defines the integrand as the wave function squared as is how probability density is defined.
     #Thank god there are no imaginary numbers here, since otherwise I would throw my laptop into a lake.
     #Setting the z to +/- R over 2 so the atoms arn't on top of each other.
     integrand = psi_2p_z(x, y, z + R/2) * psi_2p_z(x, y, z - R/2)
+
+    #Apperently you have to normalize the integrand, don't ask how long it took me to figure that out.
+    denominator = trapezoid.pdf(x,c,d,loc=0, scale=L) * trapezoid.pdf(y,c,d,loc=0, scale=L) * trapezoid.pdf(z,c,d,loc=0, scale=L)
+    norm_integ = integrand/denominator
     #Takes the integrand, multiplies it by the integration range cubed so it is a volume, then by 8 since it's semetrical on all axies
-    integral = 8 * np.mean(integrand) * (b - a)**3
+    integral = 8 * np.mean(norm_integ) * (b - a)**3
     #The same as before but for variance
-    variance = 8 * np.var(integrand) * (b - a)**3
+    variance = 8 * np.var(norm_integ) * (b - a)**3
 
     #Returns the average and standard deviation
     return integral, np.sqrt(variance)
